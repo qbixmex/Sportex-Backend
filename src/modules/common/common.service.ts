35a54b735f39f624070642c79830880a164b2659
Daniel González Briseño <qbixmex@gmail.com>
2026-09-08 13:10:07 -0600

    Version updated and Fixed un-used imports

--- src/modules/common/common.service.ts
+++ src/modules/common/common.service.ts
@@ -15,7 +15,7 @@ export class CommonService {
       this.logger.error(`📌 Database Error (TypeORM):\n${error.message}`);
       this.logger.error(`Postgres Code: ${error.driverError?.code}`);
       if (error.driverError?.code === '23505') {
-        const columnError = (error.driverError?.detail as string).split('=')[1].split(' ')[0];
+        const columnError = (error.driverError.detail as string).split('=')[1].split(' ')[0];
         const errorMessage = `${columnError} ya existe, elija otro`;
         this.logger.error(errorMessage);
         throw new BadRequestException('Database Error', errorMessage);
