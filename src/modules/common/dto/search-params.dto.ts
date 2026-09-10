9bb677c3badf4b1daa550fa8f9a00df62f0a96ab
Daniel González Briseño <qbixmex@gmail.com>
2026-09-09 19:38:23 -0600

    Renamed searchTerm by search_term
    - Renamed searchTerm by search_term property in SearchParamsDto
    - Renamed searchTerm by search_term in users service find all

--- src/modules/common/dto/search-params.dto.ts
+++ src/modules/common/dto/search-params.dto.ts
@@ -4,7 +4,7 @@ import { IsOptional, IsString, Min } from "class-validator";
 export class SearchParamsDto {
   @IsOptional()
   @IsString({ message: '¡ El término de búsqueda debe ser una cadena de texto !' })
-  searchTerm?: string;
+  search_term?: string;
 
   @IsOptional()
   @Min(1)
